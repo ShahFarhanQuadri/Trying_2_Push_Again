694aede1b4bb202930d75347392c848989ab0e62
ShahFarhanQuadri <farhanquadri017@gmail.com>
2025-08-08 18:34:34 +0530

    Function to subtract number

---
+++ SubtractANumber.py
@@ -0,0 +1,11 @@
+#Subtract A number
+
+def subtract(x,y):
+    return x-y
+
+print("Enter two numbers to subtract:")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number:"))
+z = subtract(a,b)
+print(z)
+
